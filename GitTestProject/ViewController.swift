d82255c99f665602b1834eeb4d5306112e45fd9b
Kemal Beler <>
2021-07-22 06:34:43 +0300

    second

--- GitTestProject/ViewController.swift
+++ GitTestProject/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("git 1")
+        print("git 2")
     }
 
 
