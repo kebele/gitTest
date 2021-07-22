ebb0f0fdd2e7ef5dc8d0d6f1aabaed91b2fc0be8
Kemal Beler <>
2021-07-22 06:32:54 +0300

    source tree deneme 1

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Kemal Beler on 22.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("git 1")
+    }
+
+
+}
+
